8b726038f9dc5753b9936acb14436d0acaf9aa40
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-09-01 00:23:09 +0530

    okay

--- hosted/index.js
+++ hosted/index.js
@@ -1,4 +1,22 @@
 import Redis from "ioredis";
 
-const client = new Redis(process.env.REDIS_URL);
-await client.set("foo", "bar");
+const REDIS_URL = process.env.REDIS_URL;
+// console.log("REDIS_URL", REDIS_URL);
+
+const client = new Redis(REDIS_URL);
+
+const key = "foo";
+
+async function getVal() {
+  const get = await client.get(key);
+  console.log(get);
+}
+
+async function init() {
+  await client.set(key, "bar123");
+
+  await getVal();
+}
+init().catch((e) => {
+  console.log("e", e);
+});
